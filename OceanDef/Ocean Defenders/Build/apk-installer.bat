3c5275a3fc0c99c660862738d15b80b5b8777ca9
Felipe Moreti <felipe_moreti7@hotmail.com>
2018-02-16 09:48:22 -0200

    project settings atualizada Bats para instalar o apk

---
+++ OceanDef/Ocean Defenders/Build/apk-installer.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb" uninstall com.eFlow.OceanDefenders
+"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb" install "OceanDef.apk"
+"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb" shell am start -n com.eFlow.OceanDefenders/com.unity3d.player.UnityPlayerActivity
---
+++ OceanDef/Ocean Defenders/Build/runner.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb" shell am start -n com.eFlow.OceanDefenders/com.unity3d.player.UnityPlayerActivity
+
+pause
