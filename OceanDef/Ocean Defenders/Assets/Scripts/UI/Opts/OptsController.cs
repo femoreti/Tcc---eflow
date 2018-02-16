4f6540d36e63453e0186167d0e2344d8bf978855
Felipe Moreti <felipe_moreti7@hotmail.com>
2018-02-16 00:09:32 -0200

    localizaçao e options com interação

--- OceanDef/Ocean Defenders/Assets/Scripts/UI/Opts/OptsController.cs
+++ OceanDef/Ocean Defenders/Assets/Scripts/UI/Opts/OptsController.cs
@@ -1,11 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class OptsController : MonoBehaviour {
+public class OptsController : MonoBehaviour
+{
+    private int _currSelected;
+
+    public List<Sprite> languageImages;
+    public Image LangImg;
+
 
     public void OnClose()
     {
         gameObject.SetActive(false);
+    }
+
+    public void OnLangNext()
+    {
+        _currSelected++;
+
+        if (_currSelected >= languageImages.Count)
+            _currSelected = 0;
+
+        LanguageManager.SetLanguage(_currSelected);
+        LangImg.sprite = languageImages[_currSelected];
+
+    }
+
+    public void OnLangBack()
+    {
+        _currSelected--;
+
+        if (_currSelected < 0)
+            _currSelected = languageImages.Count - 1;
+
+        LanguageManager.SetLanguage(_currSelected);
+        LangImg.sprite = languageImages[_currSelected];
     }
 }
